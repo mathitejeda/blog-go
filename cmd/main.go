70e178b51312cb374dd2144780dcbd2c5c3e52d4
mathitejeda <mathi.tejeda@gmail.com>
2024-10-22 21:29:26 -0300

    Se agregan imagenes, se configuran las rutas de public para servir mediante echo y se agrega favicon

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"blog/handlers"
 	"github.com/labstack/echo/v4"
-    "blog/handlers"
 )
 
 func main() {
-    app := echo.New()
-    app.Static("/assets", "view/static/assets")
-    handlers.RegisterRoutes(app)
-    app.Logger.Fatal(app.Start(":1323"))
+	app := echo.New()
+	app.Static("/assets", "view/static/assets")
+	app.Static("public", "view/public")
+	app.File("/favicon.ico", "view/public/favicon.ico")
+	handlers.RegisterRoutes(app)
+	app.Logger.Fatal(app.Start(":1323"))
 }
